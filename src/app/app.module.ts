bd10b402d6dcfb8237ae251bc35042aa4946bbfe
amaury2409 <amaury.henrottay@gmail.com>
2019-12-14 00:21:24 +0100

    root module

---
+++ src/app/app.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from "@angular/core";
+import { BrowserModule } from "angular/plateform-browser";
+
+import { AppComponent } from "./app.component";
+
+@NgModule({
+  imports: [BrowserModule],
+  declarations: [AppComponent],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
